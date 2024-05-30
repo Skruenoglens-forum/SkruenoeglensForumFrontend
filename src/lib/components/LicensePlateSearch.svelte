d14af692db5e42502161dd6acd12fa10988d14bb
Emil Andersen <emiand@norlys.dk>
2024-05-30 12:46:20 +0200

    fix getCarByLicensePlate and styling for car data

--- src/lib/components/LicensePlateSearch.svelte
+++ src/lib/components/LicensePlateSearch.svelte
@@ -1,8 +1,9 @@
 <script>
-	let bilIsLoading;
+	let carIsLoading;
 	let licensplateInput;
-	let bil;
+	let car;
 	async function getCarByLicensePlate(licenseplate) {
+		carIsLoading = true;
 		let res = await fetch(`https://lp.skruenøglen.dk/getcarbylp?licenseplate=${licenseplate}`, {
 			method: 'GET',
 			headers: {
@@ -11,9 +12,9 @@
 		});
 
 		if (res.status == 200) {
-			bil = await res.json();
-			console.log(bil);
+			car = await res.json();
 		}
+		carIsLoading = false;
 	}
 </script>
 
@@ -22,9 +23,10 @@
 		<h1 class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
 			Søg efter nummerplade
 		</h1>
-		<!-- <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
+		<div class="p-6 space-y-4 md:space-y-6 sm:p-8">
 			<form class="m-4 flex">
 				<input
+					disabled={carIsLoading}
 					bind:value={licensplateInput}
 					class="bg-white border border-t border-b border-l border-red-200 text-gray-900 sm:text-sm rounded-l-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5"
 					type="text"
@@ -32,56 +34,40 @@
 				/>
 				<button
 					class="px-8 rounded-r-lg bg-blue-400 text-white-800 font-bold p-4 uppercase border-red-500 border-t border-b border-r"
-					on:click={getCarByLicensePlate(licensplateInput)}>Søg</button
+					on:click={getCarByLicensePlate(licensplateInput)}>{#if !carIsLoading}Søg{:else}Henter...{/if}</button
 				>
 			</form>
-		</div> -->
+		</div>
 
-		<div class="w-full gap-2 flex flex-col justify-center items-center px-4">
-			<form class="m-4 flex">
-				<input
-					disabled={bilIsLoading}
-					bind:value={licensplateInput}
-					class="rounded-l-lg p-4 border-t mr-0 border-b border-l uppercase text-gray-800 border-red-500 bg-white"
-					placeholder="AB 12 123"
-				/>
-
-				<button
-					disabled={bilIsLoading}
-					class=" flex items-center justify-center px-8 rounded-r-lg bg-blue-400 min-w-28 text-white-800 font-bold p-4 uppercase border-red-500 border-t border-b border-r"
-					on:click={getCarByLicensePlate(licensplateInput)}
-					>{#if !bilIsLoading}Søg{/if}</button
-				>
-			</form>
-
-			{#if bil}
+		<div class="w-full gap-2 flex flex-col justify-center items-center px-4 mb-4">
+			{#if car}
 				<div class="justify-center w-full bg-white border border-gray-200 rounded-lg shadow-md p-4">
 					<h3 class="text-lg font-semibold text-gray-900 mb-4">Biloplysninger</h3>
 					<div class="flex justify-between items-center mb-2">
 						<span class="text-gray-600"><i class="fas fa-car"></i> Mærke:</span>
-						<span class="text-gray-800">{bil.brandnavn}</span>
+						<span class="text-gray-800">{car.brandnavn}</span>
 					</div>
 					<div class="flex justify-between items-center mb-2">
 						<span class="text-gray-600"><i class="fas fa-tachometer-alt"></i> Model:</span>
-						<span class="text-gray-800">{bil.modelnavn}</span>
+						<span class="text-gray-800">{car.modelnavn}</span>
 					</div>
 					<div class="flex justify-between items-center mb-2">
 						<span class="text-gray-600"><i class="fas fa-tachometer-alt"></i> Variant:</span>
-						<span class="text-gray-800">{bil.køretøjVariantnavn}</span>
+						<span class="text-gray-800">{car.køretøjVariantnavn}</span>
 					</div>
 					<div class="flex justify-between items-center mb-2">
 						<span class="text-gray-600"><i class="fas fa-calendar-alt"></i> Indregisteringsår:</span
 						>
-						<span class="text-gray-800">{new Date(bil.førsteRegistreringDato).getFullYear()}</span>
+						<span class="text-gray-800">{new Date(car.førsteRegistreringDato).getFullYear()}</span>
 					</div>
 
 					<div class="flex justify-between items-center">
 						<span class="text-gray-600"><i class="fas fa-gas-pump"></i> Brændstof:</span>
-						<span class="text-gray-800">{bil.drivkaftType}</span>
+						<span class="text-gray-800">{car.drivkaftType}</span>
 					</div>
 					<div class="flex justify-between items-center mb-2">
 						<span class="text-gray-600"><i class="fas fa-tachometer-alt"></i> VIN:</span>
-						<span class="text-gray-800">{bil.stelnummer}</span>
+						<span class="text-gray-800">{car.stelnummer}</span>
 					</div>
 				</div>
 			{/if}

bd811e1e7007bc593580d52f99ff32cdb42d7ade
Emil Andersen <emiand@norlys.dk>
2024-06-17 11:53:42 +0200

    fix

--- src/lib/components/Comments.svelte
+++ src/lib/components/Comments.svelte
@@ -232,7 +232,6 @@
 					<div class="block text-blue-600 py-2 px-4 hover:bg-gray-100">
 						<button on:click={() => {
 							toggleCommentEditTextField(comment.id)
-							toggleCommentReplyTextField(comment.id)
 						}}>Rediger</button>
 					</div>
 				</li>
